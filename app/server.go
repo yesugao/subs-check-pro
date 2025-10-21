861f789921cf71a09cb2d49a27b6475b30dc75c6
sinspired <ggmomo@gmail.com>
2025-10-21 20:43:01 +0800

    perf: 增加日志显示长度

--- app/server.go
+++ app/server.go
@@ -223,7 +223,7 @@ func (app *App) getLogs(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"logs": []string{}})
 		return
 	}
-	lines, err := ReadLastNLines(logPath, 100)
+	lines, err := ReadLastNLines(logPath, 200)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("读取日志失败: %v", err)})
 		return
